0997f0779a3f5833035d2373b301872fcbc0bb78
Ricky Dam <ricky.dam10@hotmail.com>
2016-07-31 11:07:48 -0400

    Implemented drawing canvas and mouse position label

--- Paint/Form1.Designer.cs
+++ Paint/Form1.Designer.cs
@@ -28,14 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.drawToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip2 = new System.Windows.Forms.MenuStrip();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.positionLabel = new System.Windows.Forms.ToolStripStatusLabel();
             this.menuStrip2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // toolStripMenuItem1
@@ -67,36 +71,64 @@
             this.toolStripMenuItem1});
             this.menuStrip2.Location = new System.Drawing.Point(0, 0);
             this.menuStrip2.Name = "menuStrip2";
-            this.menuStrip2.Size = new System.Drawing.Size(595, 24);
+            this.menuStrip2.Size = new System.Drawing.Size(601, 24);
             this.menuStrip2.TabIndex = 0;
             this.menuStrip2.Text = "menuStrip2";
             this.menuStrip2.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip2_ItemClicked);
             // 
             // pictureBox2
             // 
-            this.pictureBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.pictureBox2.Location = new System.Drawing.Point(12, 27);
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox2.Location = new System.Drawing.Point(0, 24);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(318, 409);
+            this.pictureBox2.Size = new System.Drawing.Size(601, 603);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
             this.pictureBox2.TabIndex = 1;
             this.pictureBox2.TabStop = false;
             this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            this.pictureBox2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox2_MouseDown);
+            this.pictureBox2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox2_MouseMove);
+            this.pictureBox2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox2_MouseUp);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.positionLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 605);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(601, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // positionLabel
+            // 
+            this.positionLabel.BackColor = System.Drawing.Color.Transparent;
+            this.positionLabel.Name = "positionLabel";
+            this.positionLabel.Size = new System.Drawing.Size(74, 17);
+            this.positionLabel.Text = "Position: 0, 0";
+            this.positionLabel.Click += new System.EventHandler(this.toolStripStatusLabel1_Click);
             // 
             // Form1
             // 
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.AutoSize = true;
-            this.ClientSize = new System.Drawing.Size(595, 513);
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(601, 627);
+            this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.menuStrip2);
             this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip2;
             this.Name = "Form1";
-            this.Text = "Paint";
+            this.Text = "Paint Replica";
             this.Load += new System.EventHandler(this.Form1_Load_1);
             this.menuStrip2.ResumeLayout(false);
             this.menuStrip2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -113,6 +145,8 @@
         private System.Windows.Forms.MenuStrip menuStrip2;
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.ToolStripMenuItem drawToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel positionLabel;
     }
 }
 
--- Paint/Form1.cs
+++ Paint/Form1.cs
@@ -1,18 +1,74 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace Paint
 {
-    public partial class Form1 : System.Windows.Forms.Form
+    public partial class Form1 : Form
     {
+        public Bitmap canvas;
+        public Graphics g;
+        public Color currentColor;
+        private Point lastPoint = Point.Empty;
+        private bool isMouseDown = new Boolean();
+        
+        // Constructor
         public Form1()
         {
             InitializeComponent();
+
+            canvas = new Bitmap(pictureBox2.Width, pictureBox2.Height);
+            pictureBox2.Image = canvas;
+            g = Graphics.FromImage(pictureBox2.Image);
         }
 
-        private void Form1_Load_1(object sender, EventArgs e) {}
+        // Actions to be performed on load
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            currentColor = Color.Black;
+            // currentColorBox.BackColor = currentColor;
+        }
+
+        private void pictureBox2_MouseDown(object sender, MouseEventArgs e)
+        {
+            lastPoint = e.Location;
+            isMouseDown = true;
+        }
+
+        private void pictureBox2_MouseMove(object sender, MouseEventArgs e)
+        {
+            positionLabel.Text = "Position: " + e.X + ", " + e.Y;
+            if (isMouseDown == true)
+            {
+                if (lastPoint != null)
+                {
+                    Pen p = new Pen(currentColor);
+                    g.SmoothingMode = SmoothingMode.AntiAlias;
+                    g.DrawLine(p, lastPoint, e.Location);
+                    lastPoint = e.Location;
+                    p.Dispose();
+                }
+            }
+            pictureBox2.Invalidate();
+        }
+
+        private void pictureBox2_MouseUp(object sender, MouseEventArgs e)
+        {
+            lastPoint = e.Location;
+            isMouseDown = false;
+        }
+
+        // Actions to be performed when the PictureBox is clicked
         private void pictureBox2_Click(object sender, EventArgs e) {}
+
+        // Actions to be performed when menu strip is clicked
         private void menuStrip2_ItemClicked(object sender, ToolStripItemClickedEventArgs e) {}
 
         // Function to be executed when the menu item "File > Open" is clicked
@@ -30,7 +86,8 @@ namespace Paint
                 // Create an Image object from the selected file
                 Image img = Image.FromFile(filename);
                 // Set the PictureBox image to a bitmap of the selected file
-                pictureBox2.Image = new Bitmap(filename);
+                Bitmap bitmap = new Bitmap(filename);
+                pictureBox2.Image = bitmap;
                 // Set the PictureBox's Height to be the same as the selected image's Height
                 pictureBox2.Height = img.Height;
                 // Set the PictureBox's Width to be the same as the selected image's Width
@@ -41,7 +98,12 @@ namespace Paint
         // Function to be executed when the menu item "File > Draw" is clicked
         private void drawToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            
+
+        }
+
+        private void toolStripStatusLabel1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
